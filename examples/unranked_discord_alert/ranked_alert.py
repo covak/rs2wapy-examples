cc5a75408a2a22e8e86b7931e7f861ea22505c17
covak <cole.novak1@live.com>
2020-03-16 01:22:40 -0600

    Update ranked_alert.py

--- examples/unranked_discord_alert/ranked_alert.py
+++ examples/unranked_discord_alert/ranked_alert.py
@@ -37,14 +37,14 @@ DISCORD_WEBHOOK_URLS = get_env_vars("DISCORD_WEBHOOK_URL_#")
 # For the pings to work the role ID must be of the following format
 # <&ROLE_ID> where ROLE_ID is the role ID you copied from Discord.
 PING_DISCORD_ROLES = [
-    "<&552280559284650016>",  # Example role ping, put your real role ID(s) here!
+    "<&ROLE_552280559284650016>",  # Example role ping, put your real role ID(s) here!
 ]
 
 # Poll values are in seconds.
 # Poll ranked status every 5 seconds.
 POLL_INTERVAL = 5
-# Alert every 15 minutes if server is unranked.
-ALERT_INTERVAL = 60 * 15
+# Alert every 5 minutes if server is unranked.
+ALERT_INTERVAL = 60 * 5
 
 
 def main():
